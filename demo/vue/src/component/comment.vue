dfd19e67042877386908701e5225ee26f7a3abd7
jiamao <haofefe@163.com>
2019-10-12 19:55:56 +0800

    up vue

---
+++ demo/vue/src/component/comment.vue
@@ -0,0 +1,19 @@
+
+import CommentItem from './commentItem.vue';
+
+export default {
+    template: `<ul>
+            <CommentItem v-for="(item, index) in items" :key="index" :index="index" v-bind="item" v-on:click-item="showUser"   >
+                {{ 'q' + index }}
+            </CommentItem>
+    </ul>`,
+    components: {
+        CommentItem
+    },
+    props: ['items'],
+    methods: {
+        showUser: function(name, e) {
+            console.log(e);
+        }
+    }
+}
---
+++ demo/vue/src/component/commentItem.vue
@@ -0,0 +1,31 @@
+
+export default {
+    template: `<li  v-bind:user="user" @click.prevent="$emit(\'click-item\', user, $event)" :style="{color: index%2==0?\'red\':\'green\'}">
+            <slot></slot>
+            <p v-html="message"></p> 
+            <br />
+            {{ message }}
+            <input v-model.lazy="message">
+        </li>`,
+    props: [ 'user', 'content', 'index' ],
+    data() {
+        return {
+            msg: this.content
+        }
+    },
+    watch: {
+        message(v) {
+            //this.content = v;
+        }
+    },
+    computed: {
+        message: {
+            get: function() {
+                return this.msg + ' test';
+            },
+            set: function(v) {
+                this.msg = v;
+            }
+        }
+    }
+}
