13f00e4c692e306e3f37a2fcc12b13418311ebd0
jiamao <haofefe@163.com>
2019-11-06 14:42:34 +0800

    pwa

--- demo/pwa/js/mian.js
+++ demo/pwa/js/mian.js
@@ -1,17 +1,21 @@
 if ('serviceWorker' in navigator) {
-  /*
+  alert('test serviceWorker');
   navigator.serviceWorker.getRegistrations().then(function (registrations) {
       for (var item of registrations) {
-          if (item.scope === 'http://localhost/fefeding/javascript/my_doc/demo/pwa/js/') {
-              item.unregister();
+          if (item.scope === 'http://localhost/fefeding/javascript/my_doc/demo/pwa/') {
+             // item.unregister();
           }
       }
       // 注销掉污染 Service Worker 之后再重新注册...
-  });*/
 
+      install();
+  });
+
+
+  function install() {
     // register service worker
     // scope很关健，只有它下面的资源才可以匹配
-    navigator.serviceWorker.register('service-worker.js?20191107', {
+    navigator.serviceWorker.register('service-worker.js?20170117', {
       //scope: 'http://localhost/fefeding/javascript/my_doc/demo/pwa/'
     }).then(reg => {
       console.log(reg);
@@ -20,3 +24,4 @@ if ('serviceWorker' in navigator) {
     });
   
   }
+}
--- demo/pwa/service-worker.js
+++ demo/pwa/service-worker.js
@@ -3,6 +3,13 @@
 const cacheName = 'v1';
 var cacheInstance = null;
 
+async function getCache() {
+    if(!cacheInstance) {
+        cacheInstance = await caches.open(cacheName);
+    }
+    return cacheInstance;
+}
+
 // 安装服务工作线程
 self.addEventListener('install', function(event){
     // 需要缓存的资源
@@ -13,11 +20,10 @@ self.addEventListener('install', function(event){
         './images/256.png',
         //'https://jiamao.github.io/jmgraph/src/jmGraph.js'
     ];
-    console.log('service worker: run into install');
+    console.log('service worker: run into install 1.1');
     
-    event.waitUntil(caches.open(cacheName).then(function(cache)
+    event.waitUntil(getCache().then(function(cache)
     {
-        cacheInstance = cache;
         cache.matchAll(cacheFiles).then(res => {
             console.log(res);
         });
@@ -41,12 +47,13 @@ self.addEventListener('install', function(event){
 // 新的service worker线程被激活（其实和离线包一样存在"二次生效"的机理）
 self.addEventListener('activate', function (event) {
     console.log('service worker: run into activate');
+    debugger;
     event.waitUntil(caches.keys().then(function (cacheNames) {
         return Promise.all(cacheNames.map(function (name) {
             // 注意这里cacheVersion也可以是一个数组
             if(name !== cacheName){
-                console.log('service worker: clear cache' + name);
-                return cacheInstance.delete(name);
+                //console.log('service worker: clear cache' + name);
+                return caches.delete(name);
             }
         }));
     }));
@@ -64,16 +71,17 @@ self.addEventListener('fetch', function (event) {
                 status = 1;
             }
         });
-
-        cacheInstance.match(event.request).then(function (response) {
-            if(status) return;
-            // 发现匹配的响应缓存
-            if(response){
-                console.log(' service worker 匹配并读取缓存：' + event.request.url);
-                //if(/\.html$/.test(event.request.url)) fetchResource(event.request); // 如果是html，依然去请求一次，缓存起来
-                resolve && resolve(response);
-                status = 1;
-            }
+        getCache().then(cache => {
+            cache.match(event.request).then(function (response) {
+                if(status) return;
+                // 发现匹配的响应缓存
+                if(response){
+                    //console.log(' service worker 匹配并读取缓存：' + event.request.url);
+                    //if(/\.html$/.test(event.request.url)) fetchResource(event.request); // 如果是html，依然去请求一次，缓存起来
+                    resolve && resolve(response);
+                    status = 1;
+                }
+            });
         });
     }));
     var p1 = fetchResource(event.request);
@@ -92,7 +100,7 @@ self.addEventListener('fetch', function (event) {
 
 function fetchResource(request) {
     request = request.clone();    
-    return fetch(request).then(function(response){
+    return fetch(request).then(async function(response){
         if(!response || response.status !== 200 || (response.type !== 'basic' && response.type !== 'cors')){
             return response;
         }
@@ -101,9 +109,8 @@ function fetchResource(request) {
             //console.log(cache);
             //cacheInstance.put(request, responseToCache);
         //});
-        if(cacheInstance) {
-            cacheInstance.put(request, responseToCache);
-        }
+        var cache = await getCache();
+        cache.put(request, responseToCache);
         return response;
     });
 }
