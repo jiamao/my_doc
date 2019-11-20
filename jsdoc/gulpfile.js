463305fa07aab0b1073b0dfa6c36b4027d252eb6
jiamao <haofefe@163.com>
2019-11-20 09:39:06 +0800

    ts doc

---
+++ jsdoc/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var typedoc = require("gulp-typedoc");
+ 
+gulp.task("default", function() {
+    return gulp
+        .src(["src/**/*.ts"])
+        .pipe(typedoc({
+            exclude:["node_modules",
+                    "**/*+(index|.worker|.e2e).ts"],
+            // TypeScript options (see typescript docs)
+            module: "commonjs",
+            target: "es5",
+            includeDeclarations: false,
+ 
+            // Output options (see typedoc docs)
+            out: "./doc",
+ 
+            // TypeDoc options (see typedoc docs)
+            name: "基金接口",
+            ignoreCompilerErrors: false,
+            version: true,
+        }));
+    });
+    
---
+++ jsdoc/src/fund.ts
@@ -0,0 +1,55 @@
+
+/**
+ * 基金实体
+ */
+interface IFund { 
+    /**
+     * 基金代码
+     * @default null
+     */
+    code: string, 
+    /**
+     * 基金名称
+     */
+    name: string,
+
+    /**
+     * 公司信息
+     */
+    company: ICompany
+}
+
+/**
+ * 公司实体
+ */
+interface ICompany {
+    /**
+     * 公司名称
+     */
+    name: string
+}
+
+/**
+ * 同步基金信息请求参数
+ */
+interface IReq {
+    /**
+     * 基金代码
+     */
+    code: string
+}
+
+/**
+ * 基金信息同步接口
+ */
+interface fund {
+    /**
+     * 同步基金信息
+     * @param code 基金代码
+     * @param [optional] req 请求参数
+     * @returns 基金实体信息，如果返回nul表示不存在
+     * @author fefeding
+     * @date 2019-11-20
+     */
+    sync_fund(code: string, req: IReq): IFund
+}
